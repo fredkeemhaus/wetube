081d295bf07e2cb6086d0957aa4c1ca1054d70df
fred <fredkeemhaus>
2018-12-24 01:44:18 +0900

    middleware part one

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const app = express();
 const PORT = 4000;
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send('Hello from home');
-const handleProfile = (req, res) => res.send("You are on my profile");
+const handleProfile = (req, res ) => res.send("You are on my profile");
+const betweenHome = (req, res, next) => {
+    console.log("Between");
+    next();
+};
 
+app.use(betweenHome);
 
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
+
 
 app.listen(PORT, handleListening);
