f8f4f08524aa12c387be725dd5fa09ec95767d16
fred <fredkeemhaus@gmail.com>
2019-01-01 21:27:20 +0900

    local variables in pug

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "../routes";
 import { 
-    users, 
     usersDetail, 
     editProfile, 
     changePassword 
@@ -9,7 +8,6 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
 userRouter.get(routes.usersDetail, usersDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
