309623986e395754e8223a7f6d4674e5f39581eb
fred <fredkeemhaus@gmail.com>
2019-01-10 21:17:31 +0900

    comment model

--- db.js
+++ db.js
@@ -1,17 +1,20 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 mongoose.connect(
-    "mongodb://localhost:27017/we-tube", 
+    process.env.MONGO_URL,
     {
         useNewUrlParser: true, 
-        useFindAndModify: false
+        useFindAndModify: false,
     }
 );
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅ Connected to DB")
-const handelError = (error) => console.log(`❌ Error on DB ConnectionL${error}`);
+const handleError = error => console.log(`❌ Error on DB Connection${error}`);
 
 db.once("open", handleOpen);
-db.on("error", handelError);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,11 +1,16 @@
+import dotenv from "dotenv";
 import "./db";
 import app from "./app";
+dotenv.config();
+import "./models/Video";
+import "./models/Comment";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
+const handleListening = () =>
+  console.log(`✅  Listening on: http://localhost:${PORT}`);
 
-app. listen(PORT, handleListening);
+app.listen(PORT, handleListening);
 
 
 
