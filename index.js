42ff1d5013e6742254825e6310dac53e5b67a128
fred <fredkeemhaus@gmail.com>
2019-04-14 21:25:54 +0900

    middleware part Two

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('h1');
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+
+const app = express()
+
+const PORT = 4000
+
+const handleListening = () => {
+  console.log(`✅Listenling on: http://localhost:${PORT}`)
+}
+const handleHome = (req, res) => {
+  res.send('Hello from home')
+}
+const handleProfile = (req, res) => {
+  res.send('You are on my Profile')
+}
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(helmet())
+app.use(morgan('dev'))
+
+app.get('/', handleHome)
+app.get('/profile', handleProfile)
+// app.get
+// app.get
+// app.get
+
+app.listen(PORT, handleListening)
